b47a10a4e6c02910c5523bfc67502bcfd494601a
Mateusz Biłgorajski <bilgorajskim@gmail.com>
2017-09-15 04:37:19 +0200

    Add missing changes

--- src/game/world.js
+++ src/game/world.js
@@ -1,27 +1,23 @@
 import World from './../engine/World.js'
-import Terrain from './../engine/objects/terrain/Terrain.js'
+import Terrain from './../engine/terrain/Terrain.js'
 import assets from './assets.js'
+import Astronaut from './objects/Astronaut.js'
 import Sky from './objects/Sky'
 import Sun from './objects/Sun'
 import Earth from './objects/Earth'
 import AmbientLight from './objects/AmbientLight'
 
-export async function buildMoonbaseWorld (scene) {
-  let terrain = await Terrain.fromImage(assets.terrain.heightMap, {
-    texture: assets.terrain.texture,
-    normalMap: assets.terrain.normalMap,
-    rocks: {
-      texture: assets.terrain.rocks.texture,
-      normalMap: assets.terrain.rocks.normalMap
-    }
-  })
-  return new World(scene, [
+export async function createMoonbaseWorld () {
+  let character = new Astronaut()
+  let terrain = await Terrain.fromImage(assets.terrain.heightMap)
+  return new World([
     new Sky(),
     new Earth(),
     new Sun(),
     new AmbientLight({
 
     }),
-    terrain
+    terrain,
+    character
   ])
 }
